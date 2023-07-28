99395728104e7a221918a2911634985fe7ea7bdd
Dan Phiffer <dan@phiffer.org>
2023-07-27 22:07:40 -0400

    lower the checkAssignments log level

--- src/controllers/calendar.ts
+++ src/controllers/calendar.ts
@@ -336,7 +336,7 @@ class Calendar {
 	}
 
 	async checkAssignments() {
-		app.log.warn('Checking assignments' + Messages.getPendingSummary());
+		app.log.info('Checking assignments' + Messages.getPendingSummary());
 		let sheets = await Sheets.getInstance();
 		let assignmentsDue = [];
 		let today = moment.default().format('YYYY-MM-DD');
